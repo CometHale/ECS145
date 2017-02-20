416a6fdbaf69596b30bb2cede9054089b6b0fcc9
Haley Sanders <haleyfsanders@gmail.com>
2017-02-20 12:28:27 -0800

    skeleton probC

---
+++ HW2/ProbC.py
@@ -0,0 +1,14 @@
+import SimPy
+
+def storesim(maxsimtime,alphac,betac,alphai,betai):
+
+	# returns the following in a tuple:
+	# 	the mean time it takes for a customer's order to be filled (0 if immediate)
+	# 	the proportion of customer orders that are filled immediately
+	# 	the proportion of inventory deliveries that are immediately used to fill a customer order upon arrival of the delivery
+	
+	global result
+	
+	result = tuple()
+	
+	return result
