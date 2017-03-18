ae483cc54562d51f280411a07e6de541804220e6
Haley Sanders <haleyfsanders@gmail.com>
2017-03-18 04:31:00 -0700

    minor changes to bintree.r

--- Final/bintree.r
+++ Final/bintree.r
@@ -2,33 +2,35 @@
 
 newbintree <- function(){
 	#max binary tree
-	tree <- matrix(c(NA,NA,NA), nrow=1,ncol=3)
-	class(tree) <- "binarytree"
-	tree
+	tree <- list()
+	class(tree) <- "bintree"
+	tree$data <- matrix(c(NA,NA,NA), nrow=1,ncol=3)
+	return(tree)
 }# constructor
 
 push.bintree <- function(tree,item,name){
 
-	left_children <- tree[,2]
-	right_children <- tree[,3]
+	left_children <- tree$data[,2]
+	right_children <- tree$data[,3]
 	missing_rchild <- which(is.na(right_children))[1]
 	missing_lchild <- which(is.na(left_children))[1]
 
-	if(is.na(tree[1,1])){ #first push
-		tree[1,1] <- item
+	if(is.na(tree$data[1,1])){ #first push
+		tree$data[1,1] <- item
 	}
 	else{
 		new_node <- c(item,NA,NA)
 
-		if(item < tree[missing_lchild,1]){ #add item as a lhs child
-			tree <- rbind(tree,new_node[])
-			tree[missing_lchild,2] <- nrow(tree)
+		if(item > tree$data[missing_rchild,1]){ #add item as a rhs child
+			tree$data <- rbind(tree$data,new_node[])
+			tree$data[missing_rchild,3] <- nrow(tree$data)
+		}
+		else if(item < tree$data[missing_lchild,1]){ #add item as a lhs child
+			tree$data <- rbind(tree$data,new_node[])
+			tree$data[missing_lchild,2] <- nrow(tree$data)
 		}
 
-		if(item > tree[missing_rchild,1]){ #add item as a rhs child
-			tree <- rbind(tree,new_node[])
-			tree[missing_rchild,3] <- nrow(tree)
-		}
+		
 	}
 
 	assign(name, tree, envir=.GlobalEnv)
@@ -36,46 +38,46 @@ push.bintree <- function(tree,item,name){
 
 pop.bintree <- function(tree,name){
 
-	if(nrow(tree) != 1){
-		values <- tree[,1] # values of all of the nodes
+	if(nrow(tree$data) - 1 > 1){
+		values <- tree$data[,1] # values of all of the nodes
 		minimum <- min(values)
 		min_row <- which(values == minimum)
-		tree <- tree[-min_row,] #remove the node representing the minimum value
+		tree$data <- tree$data[-min_row,] #remove the node representing the minimum value
 
 		#update child node row numbers
-		for(row in c(1:nrow(tree))){
-			left_child <- tree[row,2]
-			right_child <- tree[row,3]
+		for(row in c(1:nrow(tree$data))){
+			left_child <- tree$data[row,2]
+			right_child <- tree$data[row,3]
 			
 			if(!is.na(left_child)){
-				if(tree[row,2] >= min_row){
-					tree[row,2] <- NA
+				if(tree$data[row,2] >= min_row){
+					tree$data[row,2] <- NA
 				}
 			}
 			
 			if(!is.na(right_child)){
-				if(tree[row,3] >= min_row){
-					tree[row,3] <- NA
+				if(tree$data[row,3] >= min_row){
+					tree$data[row,3] <- NA
 				}
 			}
 
 		}
 
-		if( nrow(tree) >= min_row ){ #there were rows below minimum's row
+		if( nrow(tree$data) >= min_row ){ #there were rows below minimum's row
 
-			for(row in c(min_row:nrow(tree))){
-				left_children <- tree[,2]
-				right_children <- tree[,3]
+			for(row in c(min_row:nrow(tree$data))){
+				left_children <- tree$data[,2]
+				right_children <- tree$data[,3]
 
 				missing_rchild <- which(is.na(right_children))[1] #find the first row that doesn't have an rhs child
 				missing_lchild <- which(is.na(left_children))[1] #find the first row that doesn't have an lhs child
 
-				if(tree[row,1] > tree[missing_rchild,1]){
-					tree[missing_rchild,3] <- row
+				if(tree$data[row,1] > tree$data[missing_rchild,1]){
+					tree$data[missing_rchild,3] <- row
 				}
 				else if(is.na(missing_rchild)){
-					if(tree[row,1] < tree[missing_lchild,1]){
-						tree[missing_lchild,2] <- row
+					if(tree$data[row,1] < tree$data[missing_lchild,1]){
+						tree$data[missing_lchild,2] <- row
 					}
 				}
 			}
@@ -83,11 +85,11 @@ pop.bintree <- function(tree,name){
 		
 	}
 	else{ #empty binary tree
-		if(is.na(tree[1,1])){
+		if(is.na(tree$data[1,1])){
 			stop("No items in the binarytree. Stop.", call.=FALSE)
 		}
 		else{
-			tree[1,1] <- NA
+			tree$data[1,1] <- NA
 		}
 	}
 
@@ -96,15 +98,15 @@ pop.bintree <- function(tree,name){
 
 rec_print <- function(tree, row_number){
 
-	node <- tree[row_number,1]
-	left_child <- tree[row_number,2]
-	right_child <- tree[row_number,3]
+	node <- tree$data[row_number,1]
+	left_child <- tree$data[row_number,2]
+	right_child <- tree$data[row_number,3]
 
 	if(!is.na(node)){
 		print(node)	
 	}
 
-	if(row_number == nrow(tree)){ # base case
+	if(row_number == nrow(tree$data)){ # base case
 		return
 	}
 
@@ -119,8 +121,7 @@ rec_print <- function(tree, row_number){
 
 print.bintree <- function(tree){
 
-	if(is.na(tree[1,1])){
-		print("")
+	if(is.na(tree$data[1,1])){
 		return
 	}
 	
